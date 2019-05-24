eb5c4f19fa20c136b69940ad20ee2646a7d03d0a
ProjetoUdemy <b.vieira.rocha@gmail.com>
2019-05-24 11:34:01 -0300

    Deploy

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost','10.0.1.12']
+ALLOWED_HOSTS = ['localhost','clientesbvr.herokuapp.com']
 
 
 # Application definition
