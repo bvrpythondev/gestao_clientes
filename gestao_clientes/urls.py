66cd29511a761777578ea8f22e47b4152ca06ef8
ProjetoUdemy <b.vieira.rocha@gmail.com>
2019-05-25 15:32:44 -0300

    Update Django 2.1

--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(home_urls)),
     path('clientes/',include(clientes_urls)),
-    path('login/',auth_views.login,name='login'),
-    path('logout/',auth_views.logout,name='logout')
+    path('login/',auth_views.LoginView.as_view(),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout')
 ]  + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
